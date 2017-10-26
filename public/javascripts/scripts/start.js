78d5d24c589f48d157cc244d4a2c2da9fe844c64
cengels <christopherengels.de@googlemail.com>
2017-10-26 12:23:04 +0200

    Added script to start local webserver

---
+++ public/javascripts/scripts/start.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const config = require('../config');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('I am a local webserver.');
+});
+
+server.listen(config.dev.port);
