5ea826a7c0e88e486e9fb897cd06f19f86746d5d
cengels <christopherengels.de@googlemail.com>
2017-11-08 15:48:22 +0100

    Repeated form submits now possible

--- src/app/modules/routes/standard-routes.js
+++ src/app/modules/routes/standard-routes.js
@@ -1,19 +1,36 @@
 const dbQuery = require('../db/queries');
 
+const showRentalForm = (req, res, hint) => {
+    const options = {
+        data: {
+            vehicles: dbQuery.get.availableVehicles(),
+            customers: dbQuery.get.allCustomers()
+        },
+        partials: {
+            hint: hint || ''
+        }
+    };
+
+    res.render('rental-form', options);
+};
+
 module.exports = (router) => {
     return {
         rentalForm: (route) => {
-            router.get(route, (req, res) => {
-                res.render('rental-form', {
-                    data: {
-                        vehicles: dbQuery.get.availableVehicles(),
-                        customers: dbQuery.get.allCustomers()
-                    },
-                    helpers: {
-                        postRental: dbQuery.post.newRental
-                    }
-                });
-            });
+            router.get(route, (req, res) => showRentalForm(req, res));
+
+            router.post(route, (req, res) => {
+                console.log(req.body.customerid);
+                console.log(req.body.vehicleid);
+
+                if (req.body.customerid === 'default' || req.body.vehicleid === 'default') {
+                    showRentalForm(req, res, '<div class="hint-error">Please select both a customer and a vehicle.</div>');
+                } else {
+                    dbQuery.post.newRental(req.body.customerid, req.body.vehicleid, new Date().toISOString())
+                        .then(() => showRentalForm(req, res, '<div class="hint-success">Successfully booked!</div>'))
+                        .catch(() => showRentalForm(req, res, '<div class="hint-error">Oh no! An unknown error occurred. Please contact the administrator.</div>'));
+                }
+            })
         }
     }
 };
