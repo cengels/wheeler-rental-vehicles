e80a6502b83efe213ec6ca704edc9c9a18c37018
cengels <christopherengels.de@googlemail.com>
2017-11-08 11:02:15 +0100

    Added process.env.NODE_ENV variable

--- scripts/start-dev.js
+++ scripts/start-dev.js
@@ -1,4 +1,6 @@
 const config = require('../config/config');
 const app = require('../src/app/app');
 
+process.env.NODE_ENV = 'development';
+
 app.listen(config.dev.port);
--- scripts/start.js
+++ scripts/start.js
@@ -1,5 +1,7 @@
 const config = require('../config/config');
 const app = require('../src/app/app');
 
+process.env.NODE_ENV = 'production';
+
 // TODO: Register a webserver
 // app.listen(config.prod.port);
