204ee8ab48918f26c8d2a68923093c929dfa0efe
cengels <christopherengels.de@googlemail.com>
2017-11-10 10:48:32 +0100

    Added babel and build script

--- scripts/start.js
+++ scripts/start.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV = 'production';
 
 const config = require('../config/config');
-const app = require('../src/app/app');
+const app = require('../dist/app/app');
 
 app.listen(config.prod.port);
