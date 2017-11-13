aafbe992e1c8724b63dba4870b9f097e25e4184c
cengels <christopherengels.de@googlemail.com>
2017-11-13 12:48:36 +0100

    Modified deploy script because double tests are annoying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 babel src -d dist --presets es2015,stage-2
+rm -rf ./dist/app/__tests__
 cp -rf ./src/app/views ./dist/app/views
 cp -rf ./src/app/styles ./dist/app/styles
 gcloud compute scp ./dist \christopher-genesis-vm:./rental-app --recurse
--- src/app/modules/routes/where-conditions.js
+++
@@ -1,9 +0,0 @@
-module.exports = (params, query) => {
-    if (Object.keys(params).length > 0 || Object.keys(query).length > 0) {
-        const keys = Object.keys(params).concat(Object.keys(query));
-        const values = Object.values(params).concat(Object.values(query));
-        return 'WHERE ' + keys.map((key, i) => `${key} = ${values[i]}`).join(' AND ');
-    }
-
-    return '';
-};
