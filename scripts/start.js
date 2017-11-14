e1d7a6cb95f5af0d877999d9cc80762153188668
cengels <christopherengels.de@googlemail.com>
2017-11-14 10:21:27 +0100

    Updated start script to reflect server config file location

--- scripts/start.js
+++ scripts/start.js
@@ -1,4 +1,4 @@
-process.env.NODE_CONFIG_DIR = '../config';
+process.env.NODE_CONFIG_DIR = process.env.NODE_ENV === 'production' ? '/home/cengels/rental-app/config' : '../config';
 
 const config = require('config');
 const app = process.env.NODE_ENV === 'production' ? require('../dist/app/app') : require('../src/app/app');
