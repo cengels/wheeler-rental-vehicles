2e704c5ec4ab434740a03105d3f452a2bc013b9b
cengels <christopherengels.de@googlemail.com>
2017-11-15 09:46:24 +0100

    Updated deploy script to automatically create folders

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-gcloud compute scp ./dist \christopher-genesis-vm:./rental-app --recurse
+gcloud compute ssh cengels@christopher-genesis-vm --command "mkdir ./rental-app"
+gcloud compute ssh cengels@christopher-genesis-vm --command "mkdir ./rental-app/dist"
+gcloud compute ssh cengels@christopher-genesis-vm --command "mkdir ./rental-app/logs"
+gcloud compute scp ./package.json \christopher-genesis-vm:./rental-app
 gcloud compute scp ./scripts \christopher-genesis-vm:./rental-app --recurse
 gcloud compute scp ./public \christopher-genesis-vm:./rental-app --recurse
 gcloud compute scp ./config \christopher-genesis-vm:./rental-app --recurse
-gcloud compute scp ./package.json \christopher-genesis-vm:./rental-app
+gcloud compute scp ./dist/app \christopher-genesis-vm:./rental-app/dist --recurse
 
 # If IP changes (VM restarts):
 # 1. sudo vi /etc/nginx/sites-available/default (adjust host)
