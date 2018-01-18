1b42358227b03b98983ec120ee6eeb254a8d9128
Christopher Engels <christopherengels.de@googlemail.com>
2018-01-18 11:44:51 +0100

    Updated deploy script for webpack

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 gcloud compute ssh cengels@christopher-genesis-vm --command "mkdir ./rental-app"
+gcloud compute ssh cengels@christopher-genesis-vm --command "mkdir ./rental-app/public"
+gcloud compute ssh cengels@christopher-genesis-vm --command "mkdir ./rental-app/public/js"
 gcloud compute ssh cengels@christopher-genesis-vm --command "mkdir ./rental-app/dist"
 gcloud compute ssh cengels@christopher-genesis-vm --command "mkdir ./rental-app/logs"
 gcloud compute scp ./package.json \christopher-genesis-vm:./rental-app
 gcloud compute scp ./scripts \christopher-genesis-vm:./rental-app --recurse
-gcloud compute scp ./public \christopher-genesis-vm:./rental-app --recurse
+gcloud compute scp ./public/fonts \christopher-genesis-vm:./rental-app/public --recurse
+gcloud compute scp ./public/images \christopher-genesis-vm:./rental-app/public --recurse
+gcloud compute scp ./public/js/public.js \christopher-genesis-vm:./rental-app/public/js
+gcloud compute scp ./public/styles \christopher-genesis-vm:./rental-app/public --recurse
 gcloud compute scp ./config \christopher-genesis-vm:./rental-app --recurse
 gcloud compute scp ./dist/app \christopher-genesis-vm:./rental-app/dist --recurse
 
