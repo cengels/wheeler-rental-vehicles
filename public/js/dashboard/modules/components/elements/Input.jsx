f4ecb9c6a1876d6ed48cebfae477022af9f47076
Christopher Engels <christopherengels.de@googlemail.com>
2018-01-22 13:55:50 +0100

    Isolate and fix searchbar

---
+++ public/js/dashboard/modules/components/elements/Input.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class Input extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { value: '' };
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({value: event.target.value});
+	}
+
+	render() {
+		return (
+			<input id={this.props.id} name={this.props.id} type={this.props.type} value={this.state.value}
+				   placeholder={this.props.placeholder} onChange={this.handleChange}/>
+		)
+	}
+}
--- public/js/dashboard/modules/components/elements/TopBar/SearchPanel.jsx
+++ public/js/dashboard/modules/components/elements/TopBar/SearchPanel.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Input from '../Input';
 
 export default class SearchPanel extends React.Component {
 	render() {
@@ -6,7 +7,7 @@ export default class SearchPanel extends React.Component {
 			<div id="search-panel">
 				<div id="search-bar">
 					<i className="fa fa-search" aria-hidden="true"/>
-					<input id="search-input" name="search-input" type="text" value=""
+					<Input id="search-input" type="text"
 						   placeholder="Enter a license plate, a customer name, a location, ..."/>
 				</div>
 			</div>
